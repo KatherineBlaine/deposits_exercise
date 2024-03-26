f145cac9d9c88c6bacc838e48ef08e1234f01acc
Katherine Blaine <blaine.katherine12@gmail.com>
2024-03-26 17:31:18 -0500

    Add test for Deposit POST request

--- spec/requests/deposits_spec.rb
+++ spec/requests/deposits_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Deposits", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Deposits', type: :request do
+  describe 'POST /tradelines/:tradeline_id/deposits' do
+    let(:tradeline) { create(:tradeline) }
+    let(:valid_attributes) { attributes_for(:deposit) }
+
+    context 'with valid parameters' do
+      it 'creates a new deposit' do
+        expect {
+          post tradeline_deposits_path(tradeline), params: { deposit: valid_attributes }
+        }.to change(Deposit, :count).by(1)
+      end
+
+      it 'returns a success response' do
+        post tradeline_deposits_path(tradeline), params: { deposit: valid_attributes }
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'associates the deposit with the tradeline' do
+        post tradeline_deposits_path(tradeline), params: { deposit: valid_attributes }
+        expect(Deposit.last.tradeline).to eq(tradeline)
+      end
+    end
   end
 end
