1d4af3b73f0925eb064c449cc51a7ef7d622a546
Katherine Blaine <blaine.katherine12@gmail.com>
2024-03-26 17:49:23 -0500

    Delete comment in Routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   resources :tradelines do
     resources :deposits, only: [:index, :create, :show]
   end
